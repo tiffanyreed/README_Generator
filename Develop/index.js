23cfcc373d245724d63d6eea8f28a7dd73308823
tiffanyreed <tiff.reed99@gmail.com>
2023-08-27 09:43:02 -0700

    Added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,17 +1,45 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const path = require('path'); 
+const path = require('path');
 const generateMarkdown = require('../Develop/utils/generateMarkdown');
+const { type } = require('os');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: input,
+        name: readmetitle,
+        message: 'Please name your README:',
+    }
+    {
+        type: input,
+        name: purposeofproject,
+        message: 'What was the purpose for building thid project:',
+    }
+    {
+        type: input,
+        name: resolution,
+        message: 'What probelm will this project help sove:',
+    }
+    {
+        type: input,
+        name: standout,
+        message: 'What makes your project stand out from others:',
+    }
+    {
+        type: input,
+        name: contribution,
+        message: 'How can others contibute to make the code better:',
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
