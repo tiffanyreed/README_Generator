a520fe2e85bde83e834438336095bdcc6309a0c6
tiffanyreed <tiff.reed99@gmail.com>
2023-08-27 09:52:20 -0700

    added more questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,6 +4,7 @@ const inquirer = require('inquirer');
 const path = require('path');
 const generateMarkdown = require('../Develop/utils/generateMarkdown');
 const { type } = require('os');
+const { createBrotliDecompress } = require('zlib');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -11,27 +12,42 @@ const questions = [
         type: input,
         name: readmetitle,
         message: 'Please name your README:',
-    }
+    },
     {
         type: input,
         name: purposeofproject,
         message: 'What was the purpose for building thid project:',
-    }
+    },
     {
         type: input,
         name: resolution,
         message: 'What probelm will this project help sove:',
-    }
+    },
     {
         type: input,
         name: standout,
         message: 'What makes your project stand out from others:',
-    }
+    },
     {
         type: input,
-        name: contribution,
-        message: 'How can others contibute to make the code better:',
-    }
+        name: languages,
+        message: 'What languages were used in your project:',
+    },
+    {
+        type: input,
+        name: creator,
+        message: 'What is your name:',
+    }, 
+    {
+        type: input,
+        name: creatorusername,
+        message: 'What is your GitHub username:',
+    }, 
+    {
+        type: input,
+        name: collaborators,
+        message: 'List any collaborators you had:',
+    }, 
 
 ];
 
